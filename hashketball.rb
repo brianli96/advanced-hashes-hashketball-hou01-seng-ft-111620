a8ff30bb11a2f161bdaab7b238d1cbe0aea8f1f4
Brian Li <brian.li96@yahoo.com>
2020-11-08 22:26:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -167,3 +167,13 @@ def player_numbers(team_name)
   end
 end
 
+def player_stats(player_n)
+  game_hash.each do |home_away, keys|
+    keys[:players].each do |player|
+      if player[:player_name] == player_n
+        return player.delete_if { |stat, value| [:player_name].include?(stat)}
+      end
+    end
+  end
+end
+
